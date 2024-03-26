8268565a09f6acfadd54b2b165845aa9a5ad2bd7
Yuuuki2521 <158004195+Yuuuki2521@users.noreply.github.com>
2024-03-26 13:36:05 -0400

    Add files via upload

---
+++ signUp.js
@@ -0,0 +1,10 @@
+function showLogin() {
+    document.getElementById('LoginForm').style.display = 'flex';
+    document.getElementById('SignUpForm').style.display = 'none';
+  }
+  
+  function showSignup() {
+    document.getElementById('SignUpForm').style.display = 'flex';
+    document.getElementById('LoginForm').style.display = 'none';
+  }
+  
