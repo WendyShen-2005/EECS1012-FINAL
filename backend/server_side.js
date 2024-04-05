2a263fb4215177aec19044fe236950f0dd0b77e4
Wendy <wendyshen2005@gmail.com>
2024-04-05 00:40:41 -0400

    added save function for background & profile picture

--- backend/server_side.js
+++ backend/server_side.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const fs = require("fs");
+
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -21,14 +23,92 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-// Route for handling file uploads
-app.post('/api/upload', upload.single('bgImg'), (req, res) => {
-    res.send("Picture uploaded successfully")
+// Routes for handling file uploads
+app.post('/api/upload/bgImg', upload.single('bgImg'), (req, res) => {
+    res.send("Picture uploaded successfully (close this tab)");
 });
 
-const port = 3000;
+app.post('/api/upload/pfp', upload.single('pfp'), (req, res) => {
+    res.send("Picture uploaded successfully (close this tab)");
+})
+
+//Routes for saving bg preferences
+app.post('/post', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    console.log(req.query)
+    var queryInfo = JSON.parse(req.query['data']);
+
+    if(queryInfo['action'] == 'setBgImg'){
+        fs.readFile('./backend/profiles-list.json', 'utf-8', (err, jsonString) => {
+            errPrint(err);
+            try {
+                var profiles = JSON.parse(jsonString);
+                for(var i = 0; i < profiles.length; i++)
+                    if(profiles[i].username == queryInfo['name']){
+                        profiles[i].bgSetting = "img";
+                        profiles[i].bgImg = queryInfo['imgName'];
+                    }
+                fileWriter(profiles);
+            } catch(err){
+                console.log(err);
+            }
+        })
+        saved(res);
+    } else if(queryInfo['action'] == 'setBGColor'){
+        fs.readFile('./backend/profiles-list.json', 'utf-8', (err, jsonString) => {
+            errPrint(err);
+            try {
+                var profiles = JSON.parse(jsonString);
+                for(var i = 0; i < profiles.length; i++)
+                    if(profiles[i].username == queryInfo['name']){
+                        profiles[i].bgSetting = "color";
+                        profiles[i].bgColor = queryInfo['color'];
+                    }
+                fileWriter(profiles);
+            } catch(err){
+                console.log(err);
+            }
+        })
+        saved(res);
+    } else if(queryInfo['action'] == 'setPFP'){
+        fs.readFile('./backend/profiles-list.json', 'utf-8', (err, jsonString) => {
+            errPrint(err);
+            try {
+                var profiles = JSON.parse(jsonString);
+                for(var i = 0; i < profiles.length; i++)
+                    if(profiles[i].username == queryInfo['name'])
+                        profiles[i].pfp = queryInfo['imgName'];
+                fileWriter(profiles);
+            } catch(err){
+                console.log(err);
+            }
+        })
+        saved(res);
+    }
+})
 
 // Start the server
-app.listen(port, () => {
+app.listen(3000, () => {
     console.log("Listening on port 3000");
 });
+
+saved = (res) => {
+    console.log("saved")
+    var jsontext = JSON.stringify({
+        'action': 'saved'
+    })
+    res.send(jsontext);
+} 
+fileWriter = (profiles) => {
+    profiles = JSON.stringify(profiles, null, 2);
+        
+    fs.writeFile('./backend/profiles-list.json', profiles, err => {
+        if(err) {
+            console.log(err);
+        }
+    })
+}
+errPrint = (err) => {
+    if(err)
+        console.log(err);
+}
--- scripts/profile-editor.js
+++ scripts/profile-editor.js
@@ -3,13 +3,53 @@ var editBg = false;
 
 saveBGColor = () => {
     const bg = document.getElementById("profile");
-    const color = document.getElementById("color-picker").value;
-    bg.style.backgroundColor = color + "";
+    bg.style.backgroundImage = null;
+    var color = document.getElementById("color-picker").value + "";
+    bg.style.backgroundColor = color;
+    $.post(url+'?data='+JSON.stringify({
+        'name':'test',
+        'action':'setBGColor',
+        'color':'/'+color
+    }),response);
 }
 setBgImg = () => {
     const bg = document.getElementById("profile");
+    bg.style.backgroundColor = null;
     var fileName = document.getElementById('bgImg').value + "";
     fileName = fileName.substring(fileName.lastIndexOf('\\') + 1, fileName.length);
     console.log(fileName);
     bg.style.backgroundImage = `url("images/${fileName}")`;
+    $.post(url+'?data='+JSON.stringify({
+        'name':'test',
+        'action':'setBgImg',
+        'imgName':fileName
+    }),response);
+}
+setPFP = () => {
+    const pfp = document.getElementById("profile-picture");
+    var fileName = document.getElementById('pfp').value + "";
+    fileName = fileName.substring(fileName.lastIndexOf('\\') + 1, fileName.length);
+    console.log(fileName);
+    pfp.src = `images/${fileName}`;
+    $.post(url+'?data='+JSON.stringify({
+        'name':'test',
+        'action':'setPFP',
+        'imgName':fileName
+    }),response);
+}
+clearPFP = () => {
+    const pfp = document.getElementById("profile-picture");
+    pfp.src = `images/default.jpg`;
+    $.post(url+'?data='+JSON.stringify({
+        'name':'test',
+        'action':'setPFP',
+        'imgName':'default.jpg'
+    }),response);
+}
+
+function response(data, status){
+    var response = JSON.parse(data);
+
+    if(response['action'] == 'saved')
+        console.log("saved");
 }
