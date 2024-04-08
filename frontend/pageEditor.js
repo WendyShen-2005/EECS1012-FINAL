8cfdb4841e10697ad9600b4cf195f5d5eec283c8
quinnvx <151248400+quinnvx@users.noreply.github.com>
2024-04-08 01:51:09 -0400

    Add files via upload

--- frontend/pageEditor.js
+++ frontend/pageEditor.js
@@ -20,28 +20,67 @@ function centerAlign(){
 function rightAlign(){
     document.getElementById("textarea1").style.textAlign = "right";
 }
+function justifyAlign() {
+    document.getElementById("textarea1").style.textAlign = "justify";
+    document.getElementById("textarea1").style.textJustify = "inter-word";
+}
 function italic() {
     document.getElementById("textarea1").style.fontStyle = "italic";
 }
 function underline() {
     document.getElementById("textarea1").style.textDecoration = "underline";
 }
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  // Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+if (!event.target.matches('.btnFontChange')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+    }
+    }
+}
+} 
+
+function times() {
+    document.getElementById("textarea1").style.fontFamily = "Times New Roman";
+}
+function georgia() {
+    document.getElementById("textarea1").style.fontFamily = "Georgia";
+}
+function garamond() {
+    document.getElementById("textarea1").style.fontFamily = "Garamond";
+}
+function arial() {
+    document.getElementById("textarea1").style.fontFamily = "Arial";
+}
+
 function save() {
-    var a = document.getElementById("textarea1").value;
-    var text={content: a};
+    var text={content: document.getElementById("textarea1").value,
+              title: document.getElementById("title").value};
     //text["content"] = "a";
     //document.getElementById("abc").innerHTML = text["content"];
     //text["content"] = document.getElementById("textarea1").innerHTML;
     
     $.getJSON("http://localhost:3000/saveDraft", text, function(data) {
-        var inform = "<b>Data received from the server:</b><br>";
-        for (const [key, value] of Object.entries(data)) {
-            console.log(key, value);
-            inform += key + ": " + value + "<br>"
-        }
-        document.getElementById("inform").innerHTML = inform;
+        alert("Your post has been saved.")
+    })
+}
+
+function publish() {
+    var text={content: document.getElementById("textarea1").value,
+              title: document.getElementById("title").value};
+    
+    save();
+
+    $.getJSON("http://localhost:3000/publishPost", text, function(data) {
+        alert("Your post has been published.")
     })
 
 }
-
 
