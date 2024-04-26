04b211f2972fbe10d5ada6b5cecb56fc37adb401
quinnvx <151248400+quinnvx@users.noreply.github.com>
2024-04-26 11:20:35 -0400

    Add files via upload

--- merged/frontend/contact.js
+++ merged/frontend/contact.js
@@ -1,10 +1,11 @@
 function submit() {
+    //package contact info
     var info = {};
-    info["username"] = document.getElementById("username").value;
+    info["username"] = document.getElementById("username").value; 
     info["email"] = document.getElementById("email").value;
     info["description"] = document.getElementById("desc").value;
 
-    $.getJSON("http://localhost:3000/saveContact", info, function(data) {
-        alert("Your post has been published.");
+    $.getJSON("http://localhost:3000/saveContact", info, function(data) { //send data to server
+        alert("Your post has been published."); //alert if successfully conducted
 })
 }
